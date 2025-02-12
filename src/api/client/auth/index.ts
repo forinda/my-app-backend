dbac3a45048dd1d4490d44ca815bf226dbd21751
Felix Orinda <forinda82@gmail.com>
2025-02-13 01:23:42 +0300

    Add logout

--- src/api/client/auth/index.ts
+++ src/api/client/auth/index.ts
@@ -3,19 +3,19 @@ import { Router } from 'express';
 import { LoginUserController } from './controllers/login';
 import { GetUserSessionController } from './controllers/session.controller';
 import { RegisterUserController } from './controllers/register';
+import { UserLogoutController } from './controllers/logout';
 
 type Props = {
   app: Router;
 };
 
 export function setupAuthRoutes({ app }: Props) {
-  // console.log('setupUsersRoutes');
-
   const router = Router();
 
   router
     .post('/login', di.resolve(LoginUserController).post)
     .post('/session', di.resolve(GetUserSessionController).post)
+    .post('/logout', di.resolve(UserLogoutController).post)
     .post('/register', di.resolve(RegisterUserController).post);
 
   app.use('/auth', router);
