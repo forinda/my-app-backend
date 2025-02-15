b682b3140356f79a3ff3c702d414b33b27f11ef9
Felix Orinda <forinda82@gmail.com>
2025-02-15 10:10:15 +0300

    Update relations

--- src/db/schema/auth-module.ts
+++ src/db/schema/auth-module.ts
@@ -1,5 +1,7 @@
 import { getTableTimestamps } from '@/common/utils/drizzle';
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { AuthPermission } from './auth-permission';
+import { relations } from 'drizzle-orm';
 
 export const AuthModule = pgTable('auth_modules', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -8,6 +10,6 @@ export const AuthModule = pgTable('auth_modules', {
   ...getTableTimestamps()
 });
 
-// export const authModuleRelations = relations(AuthModule, ({ many }) => ({
-//   permissions: many(AuthPermission)
-// }));
+export const authModuleRelations = relations(AuthModule, ({ many }) => ({
+  permissions: many(AuthPermission)
+}));
