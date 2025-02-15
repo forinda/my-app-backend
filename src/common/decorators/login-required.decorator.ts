eb5712b9084d7ba7262324c48ed0668611bc9be1
Felix Orinda <forinda82@gmail.com>
2025-02-15 22:45:10 +0300

    Disable login required decorator

--- src/common/decorators/login-required.decorator.ts
+++ src/common/decorators/login-required.decorator.ts
@@ -1,49 +1,53 @@
-import type { ApiRequestContext } from '../interfaces/controller';
-import type { AuthorityType } from '../constants/persmission-table';
-import { getSessionUser } from '../utils/get-sesion-user';
+// import type { ApiRequestContext } from '../interfaces/controller';
+// import type { AuthorityType } from '../constants/persmission-table';
+// import { getSessionUser } from '../utils/get-sesion-user';
 
-export function LoginRequired(authority?: AuthorityType | AuthorityType[]) {
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value;
+// export function LoginRequired(authority?: AuthorityType | AuthorityType[]) {
+//   return function (
+//     target: any,
+//     propertyKey: string,
+//     descriptor: PropertyDescriptor
+//   ) {
+//     const originalMethod = descriptor.value;
 
-    descriptor.value = async function (...args: any[]) {
-      const { req } = args[0] as ApiRequestContext;
+//     descriptor.value = async function (...args: any[]) {
+//       const { req } = args[0] as ApiRequestContext;
 
-      const user = await getSessionUser(req, { throwError: true });
+//       const user = await getSessionUser(req, { throwError: true });
 
-      args[0].user = user;
-      if (user?.is_admin) {
-        return originalMethod.apply(this, args);
-      }
-      // const authManager = di.resolve(AuthorityManager);
-      // const permissions = authManager.validateAuthority(authority);
-      // const userPermissions = user.user_roles.flatMap(
-      //   ({ role: { role_permissions } }) =>
-      //     role_permissions.map(({ permission: { name } }) => name)
-      // );
+//       args[0].user = user;
+//       if (user!.sessions.length > 0) {
+//         args[0].organization_id =
+//           user!.sessions[0].auth_session_organization_id;
+//       }
+//       if (user?.is_admin) {
+//         return originalMethod.apply(this, args);
+//       }
+//       // const authManager = di.resolve(AuthorityManager);
+//       // const permissions = authManager.validateAuthority(authority);
+//       // const userPermissions = user.user_roles.flatMap(
+//       //   ({ role: { role_permissions } }) =>
+//       //     role_permissions.map(({ permission: { name } }) => name)
+//       // );
 
-      // if (permissions.length < 1) {
-      //   return originalMethod.apply(this, args);
-      // }
+//       // if (permissions.length < 1) {
+//       //   return originalMethod.apply(this, args);
+//       // }
 
-      // const hasAccess = permissions.every((permission) =>
-      //   userPermissions.includes(permission)
-      // );
+//       // const hasAccess = permissions.every((permission) =>
+//       //   userPermissions.includes(permission)
+//       // );
 
-      // if (!hasAccess) {
-      //   throw new ApiError(
-      //     'You are not authorized to see this resource',
-      //     HttpStatus.FORBIDDEN
-      //   );
-      // }
+//       // if (!hasAccess) {
+//       //   throw new ApiError(
+//       //     'You are not authorized to see this resource',
+//       //     HttpStatus.FORBIDDEN
+//       //   );
+//       // }
 
-      return originalMethod.apply(this, args);
-    };
+//       return originalMethod.apply(this, args);
+//     };
 
-    return descriptor;
-  };
-}
+//     return descriptor;
+//   };
+// }
