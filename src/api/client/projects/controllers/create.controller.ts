95b9065f9d72d4e17ee216079a3d3be20d029671
Felix Orinda <forinda82@gmail.com>
2025-02-27 12:17:29 +0300

    Create project  fixes #21

---
+++ src/api/client/projects/controllers/create.controller.ts
@@ -0,0 +1,37 @@
+import { BasePostController } from '@/common/bases/controller';
+import {
+  Controller,
+  ApiControllerMethod
+} from '@/common/decorators/controller.decorator';
+import { Dependency } from '@/common/di';
+import type { ApiRequestContext } from '@/common/interfaces/controller';
+import { inject, injectable } from 'inversify';
+
+import type { NewProjectPayload } from '../schema/schema';
+import { newProjectSchema } from '../schema/schema';
+import { CreateProjectService } from '../services/create.service';
+import { userAudit } from '@/common/utils/user-request-audit';
+import { createHttpResponse } from '@/common/utils/responder';
+
+@injectable()
+@Dependency()
+@Controller()
+export class CreateProjectController extends BasePostController {
+  @inject(CreateProjectService)
+  private service: CreateProjectService;
+  @ApiControllerMethod({
+    bodySchema: newProjectSchema,
+    pathParamTransform: {},
+    auth: true,
+    audit: userAudit('create'),
+    bodyBindOrgId: true
+  })
+  async post({ res, body }: ApiRequestContext<NewProjectPayload>) {
+    const feedback = await this.service.create({ data: body! });
+
+    return createHttpResponse(res, {
+      ...feedback,
+      statusCode: feedback.status
+    });
+  }
+}
---
+++ src/api/client/projects/services/create.service.ts
@@ -0,0 +1,59 @@
+import { injectable } from 'inversify';
+import type { NewProjectPayload } from '../schema/schema';
+
+import { and, eq } from 'drizzle-orm';
+
+import { HttpStatus } from '@/common/http';
+import { Dependency } from '@/common/di';
+import {
+  TransactionalService,
+  type TransactionContext
+} from '@/common/decorators/service-transaction';
+import { ApiError } from '@/common/errors/base';
+import type { InsertOrgProjectInterface } from '@/db/schema';
+import { OrgProject, OrgProjectCategory } from '@/db/schema';
+
+@injectable()
+@Dependency()
+export class CreateProjectService {
+  @TransactionalService()
+  async create({ data, transaction }: TransactionContext<NewProjectPayload>) {
+    const existingProject = await transaction!.query.OrgProject.findFirst({
+      where: and(
+        eq(OrgProject.name, data.name),
+        eq(OrgProject.organization_id, data.organization_id)
+      )
+    });
+
+    if (existingProject) {
+      throw new ApiError(
+        'Project with same name already exists',
+        HttpStatus.CONFLICT,
+        {}
+      );
+    }
+
+    const category = await transaction!.query.OrgProjectCategory.findFirst({
+      where: and(
+        eq(OrgProjectCategory.id, data.category_id),
+        eq(OrgProjectCategory.organization_id, data.organization_id)
+      )
+    });
+
+    if (!category) {
+      throw new ApiError('Category not found', HttpStatus.NOT_FOUND, {});
+    }
+
+    await transaction!
+      .insert(OrgProject)
+      .values(data as InsertOrgProjectInterface)
+      .returning()
+      .execute();
+
+    return {
+      data: {},
+      status: HttpStatus.CREATED,
+      message: 'Project created successfully'
+    };
+  }
+}
