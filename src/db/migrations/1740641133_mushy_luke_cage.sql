227b7fce9fb93ca11cd474a373b23ebf74d3277f
Felix Orinda <forinda82@gmail.com>
2025-02-27 10:42:44 +0300

    Add workspace model #15

---
+++ src/db/migrations/1740641133_mushy_luke_cage.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS "organization_workspaces" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "organization_workspaces_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"uuid" uuid DEFAULT gen_random_uuid() NOT NULL,
+	"organization_id" integer NOT NULL,
+	"name" varchar NOT NULL,
+	"description" varchar NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL,
+	"created_by" integer NOT NULL,
+	"updated_by" integer NOT NULL,
+	"deleted_by" integer,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp,
+	CONSTRAINT "organization_workspaces_uuid_unique" UNIQUE("uuid")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "organization_workspaces" ADD CONSTRAINT "organization_workspaces_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE restrict ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "organization_workspaces" ADD CONSTRAINT "organization_workspaces_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE restrict ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "organization_workspaces" ADD CONSTRAINT "organization_workspaces_updated_by_users_id_fk" FOREIGN KEY ("updated_by") REFERENCES "public"."users"("id") ON DELETE restrict ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "organization_workspaces" ADD CONSTRAINT "organization_workspaces_deleted_by_users_id_fk" FOREIGN KEY ("deleted_by") REFERENCES "public"."users"("id") ON DELETE restrict ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
