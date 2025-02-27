97392f47bf5f69c1f36bf64111ab079ae3bc431f
Felix Orinda <forinda82@gmail.com>
2025-02-27 12:19:32 +0300

    Query projects fixes #26

---
+++ src/api/client/projects/controllers/fetch.controller.ts
@@ -0,0 +1,29 @@
+import { BaseGetController } from '@/common/bases/controller';
+import {
+  Controller,
+  ApiControllerMethod
+} from '@/common/decorators/controller.decorator';
+import { Dependency } from '@/common/di';
+import type { ApiRequestContext } from '@/common/interfaces/controller';
+import { inject, injectable } from 'inversify';
+import { FetchProjectsService } from '../services/fetch.service';
+import { createHttpResponse } from '@/common/utils/responder';
+
+@injectable()
+@Dependency()
+@Controller()
+export class FetchProjectController extends BaseGetController {
+  @inject(FetchProjectsService)
+  private service: FetchProjectsService;
+
+  @ApiControllerMethod({
+    paginate: true,
+    auth: true,
+    bodyBindOrgId: true
+  })
+  async get({ res, pagination, organization_id }: ApiRequestContext) {
+    const feed = await this.service.get(organization_id!, pagination);
+
+    return createHttpResponse(res, { ...feed, statusCode: feed.status });
+  }
+}
---
+++ src/api/client/projects/services/fetch.service.ts
@@ -0,0 +1,31 @@
+import { injectable } from 'inversify';
+import { OrgProject } from '@/db/schema';
+import { useDrizzle } from '@/db';
+import { HttpStatus } from '@/common/http';
+import { Dependency } from '@/common/di';
+import type { ApiPaginationParams } from '@/common/utils/pagination';
+import { asc, eq } from 'drizzle-orm';
+
+@injectable()
+@Dependency()
+export class FetchProjectsService {
+  async get(organization_id: number, _?: ApiPaginationParams) {
+    const db = useDrizzle();
+
+    const depts = await db.query.OrgProject.findMany({
+      where: eq(OrgProject.organization_id, organization_id),
+      with: {
+        members: {}
+      },
+      limit: _?.limit,
+      offset: _?.offset,
+      orderBy: [asc(OrgProject.created_at)]
+    });
+
+    return {
+      data: depts,
+      message: 'Projects fetched successfully',
+      status: HttpStatus.OK
+    };
+  }
+}
