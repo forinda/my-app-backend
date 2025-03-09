f401f435c8ca251976edd5b620d92c1ac385bced
Felix Orinda <forinda82@gmail.com>
2025-03-09 09:28:39 +0300

    Fetch tasks

---
+++ src/api/client/tasks/controllers/fetch.controller.ts
@@ -0,0 +1,29 @@
+import { BaseGetController } from '@/common/bases/controller';
+import {
+  Controller,
+  ApiControllerMethod
+} from '@/common/decorators/controller.decorator';
+import { Dependency } from '@/common/di';
+import type { ApiRequestContext } from '@/common/interfaces/controller';
+import { inject, injectable } from 'inversify';
+import { FetchTasksService } from '../services/fetch.service';
+import { createHttpResponse } from '@/common/utils/responder';
+
+@injectable()
+@Dependency()
+@Controller()
+export class FetchTaskController extends BaseGetController {
+  @inject(FetchTasksService)
+  private service: FetchTasksService;
+
+  @ApiControllerMethod({
+    paginate: true,
+    auth: true,
+    bodyBindOrgId: true
+  })
+  async get({ res, pagination, organization_id }: ApiRequestContext) {
+    const feed = await this.service.get(organization_id!, pagination);
+
+    return createHttpResponse(res, { ...feed, statusCode: feed.status });
+  }
+}
---
+++ src/api/client/tasks/services/fetch.service.ts
@@ -0,0 +1,33 @@
+import { injectable } from 'inversify';
+import { OrgTask } from '@/db/schema';
+import { useDrizzle } from '@/db';
+import { HttpStatus } from '@/common/http';
+import { Dependency } from '@/common/di';
+import type { ApiPaginationParams } from '@/common/utils/pagination';
+import { asc, eq } from 'drizzle-orm';
+
+@injectable()
+@Dependency()
+export class FetchTasksService {
+  async get(organization_id: number, _?: ApiPaginationParams) {
+    const db = useDrizzle();
+
+    const depts = await db.query.OrgTask.findMany({
+      where: eq(OrgTask.organization_id, organization_id),
+      with: {
+        parent: true,
+        subtasks: true,
+        assignee: true
+      },
+      limit: _?.limit,
+      offset: _?.offset,
+      orderBy: [asc(OrgTask.created_at)]
+    });
+
+    return {
+      data: depts,
+      message: 'Tasks fetched successfully',
+      status: HttpStatus.OK
+    };
+  }
+}
