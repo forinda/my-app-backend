1363d02dca90dbbbf4c5dd23b14bb25773e1c680
Felix Orinda <forinda82@gmail.com>
2025-03-09 09:25:36 +0300

    Refactor routes with autoload array

---
+++ src/api/client/index.ts
@@ -0,0 +1,25 @@
+import type { RouteSetupFunction } from '@/common/interfaces/controller';
+import { setupAuthRoutes } from './auth';
+import { setupOrganizationRoutes } from './organizations';
+import { setupDepartmentRoutes } from './department';
+import { setupOrganizationDesignationsRoutes } from './organization-designations';
+import { setupOrganizationMemberRoutes } from './organization-members';
+import { setupDepartmentTitleRoutes } from './department-title';
+import { setupDepartmentRolesRoutes } from './department-roles';
+import { setupWorkspaceRoutes } from './workspaces';
+import { setupProjectRoutes } from './projects';
+import { setupProjectCategoriesRoutes } from './project-categories';
+import { setupTaskRoutes } from './tasks';
+export const apiFunctions: RouteSetupFunction[] = [
+  setupAuthRoutes,
+  setupOrganizationRoutes,
+  setupDepartmentRoutes,
+  setupOrganizationDesignationsRoutes,
+  setupOrganizationMemberRoutes,
+  setupDepartmentTitleRoutes,
+  setupDepartmentRolesRoutes,
+  setupWorkspaceRoutes,
+  setupProjectRoutes,
+  setupProjectCategoriesRoutes,
+  setupTaskRoutes
+];
--- src/api/v1.ts
+++ src/api/v1.ts
@@ -5,17 +5,9 @@ import { HttpStatus } from '@/common/http';
 import type { Application } from 'express';
 import { Router } from 'express';
 import { injectable } from 'inversify';
-import { setupAuthRoutes } from './client/auth';
-import { setupOrganizationRoutes } from './client/organizations';
-import { setupDepartmentRoutes } from './client/department';
-import { setupOrganizationDesignationsRoutes } from './client/organization-designations';
-import { setupOrganizationMemberRoutes } from './client/organization-members';
-import { setupDepartmentTitleRoutes } from './client/department-title';
-import { setupDepartmentRolesRoutes } from './client/department-roles';
-import { setupWorkspaceRoutes } from './client/workspaces';
+
+import { apiFunctions } from './client';
 import { createHttpResponse } from '@/common/utils/responder';
-import { setupProjectRoutes } from './client/projects';
-import { setupProjectCategoriesRoutes } from './client/project-categories';
 type Versions = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 export type ApiVersions = `/api/v${Versions}`;
 
@@ -36,16 +28,8 @@ export class ApiV1 {
         statusCode: HttpStatus.OK
       });
     });
-    setupAuthRoutes({ app: this.router });
-    setupOrganizationRoutes({ app: this.router });
-    setupDepartmentRoutes({ app: this.router });
-    setupOrganizationDesignationsRoutes({ app: this.router });
-    setupOrganizationMemberRoutes({ app: this.router });
-    setupDepartmentTitleRoutes({ app: this.router });
-    setupDepartmentRolesRoutes({ app: this.router });
-    setupWorkspaceRoutes({ app: this.router });
-    setupProjectRoutes({ app: this.router });
-    setupProjectCategoriesRoutes({ app: this.router });
+
+    apiFunctions.forEach((setup) => setup({ app: this.router }));
     app.use(this.version, this.router);
   }
 }
