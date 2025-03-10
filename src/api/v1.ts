7e06813ef5648a8b04a5ac664c35a5517acadbf8
Felix Orinda <forinda82@gmail.com>
2025-03-10 15:21:49 +0300

    Add helth check endpoint to the service

--- src/api/v1.ts
+++ src/api/v1.ts
@@ -1,13 +1,13 @@
 // import { Router } from "express";
 
 import { Dependency } from '@/common/di';
+import type { ApiReq, ApiRes } from '@/common/http';
 import { HttpStatus } from '@/common/http';
 import type { Application } from 'express';
 import { Router } from 'express';
 import { injectable } from 'inversify';
 
 import { apiFunctions } from './client';
-import { createHttpResponse } from '@/common/utils/responder';
 type Versions = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 export type ApiVersions = `/api/v${Versions}`;
 
@@ -21,15 +21,17 @@ export class ApiV1 {
     this.router = Router();
     this.version = '/api/v1';
   }
-  setup(app: Application) {
-    this.router.get('/', (req, res) => {
-      return createHttpResponse(res, {
-        message: 'API works',
-        statusCode: HttpStatus.OK
-      });
+  healthCheck(req: ApiReq, res: ApiRes) {
+    return res.status(HttpStatus.OK).json({
+      message: 'API is up and running',
+      status: HttpStatus.OK
     });
+  }
+  setup(app: Application) {
+    this.router.get('/', this.healthCheck);
 
     apiFunctions.forEach((setup) => setup({ app: this.router }));
+    app.get('/', this.healthCheck);
     app.use(this.version, this.router);
   }
 }
