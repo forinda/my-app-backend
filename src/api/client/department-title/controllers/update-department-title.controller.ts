3596de536baa0a0478a734befecbf830e4b66268
Felix Orinda <forinda82@gmail.com>
2025-03-16 10:05:10 +0300

    Refactor UpdateDepartmentTitleController and CreateNewDepartmentTitleService to remove unused dependency injection decorators

--- src/api/client/department-title/controllers/update-department-title.controller.ts
+++ src/api/client/department-title/controllers/update-department-title.controller.ts
@@ -3,9 +3,8 @@ import {
   Controller,
   ApiControllerMethod
 } from '@/common/decorators/controller.decorator';
-import { Dependency } from '@/common/di';
 import type { ApiRequestContext } from '@/common/interfaces/controller';
-import { inject, injectable } from 'inversify';
+import { inject } from 'inversify';
 
 import type { UpdateDepartmentTitleRequest } from '../schema/schema';
 import { updateDepartmentTitleSchema } from '../schema/schema';
@@ -13,8 +12,6 @@ import { UpdateDepartmentService } from '../services/update-department.service';
 import { userAudit } from '@/common/utils/user-request-audit';
 import { createHttpResponse } from '@/common/utils/responder';
 
-@injectable()
-@Dependency()
 @Controller()
 export class UpdateDepartmentTitleController extends BasePutController {
   @inject(UpdateDepartmentService)
--- src/api/client/department-title/services/create-department-title.service.ts
+++ src/api/client/department-title/services/create-department-title.service.ts
@@ -1,10 +1,9 @@
-import { injectable } from 'inversify';
 import type { DepartmentTitleCreationRequest } from '../schema/schema';
 
 import { and, eq } from 'drizzle-orm';
 
 import { HttpStatus } from '@/common/http';
-import { Dependency } from '@/common/di';
+import { dependency } from '@/common/di';
 import {
   TransactionalService,
   type TransactionContext
@@ -13,8 +12,7 @@ import { ApiError } from '@/common/errors/base';
 import type { InsertDepartmentTitleInterface } from '@/db/schema';
 import { DepartmentTitle } from '@/db/schema';
 
-@injectable()
-@Dependency()
+@dependency()
 export class CreateNewDepartmentTitleService {
   @TransactionalService()
   async create({
