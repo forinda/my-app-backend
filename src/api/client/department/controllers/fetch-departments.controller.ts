3deb380634c3630638362216cfcca974dbeb7683
Felix Orinda <forinda82@gmail.com>
2025-03-16 10:04:36 +0300

    Refactor FetchDepartmentsController and UpdateDepartmentController to remove unused dependency injection decorators

--- src/api/client/department/controllers/fetch-departments.controller.ts
+++ src/api/client/department/controllers/fetch-departments.controller.ts
@@ -3,14 +3,11 @@ import {
   Controller,
   ApiControllerMethod
 } from '@/common/decorators/controller.decorator';
-import { Dependency } from '@/common/di';
 import type { ApiRequestContext } from '@/common/interfaces/controller';
-import { inject, injectable } from 'inversify';
+import { inject } from 'inversify';
 import { FetchDepartmentService } from '../services/fetch-department.service';
 import { createHttpResponse } from '@/common/utils/responder';
 
-@injectable()
-@Dependency()
 @Controller()
 export class FetchDepartmentsController extends BaseGetController {
   @inject(FetchDepartmentService)
