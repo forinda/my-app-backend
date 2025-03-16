26d4946553336f74f462ee97b4d1a817dd8ba29e
Felix Orinda <forinda82@gmail.com>
2025-03-16 09:54:24 +0300

    Refactor dependency injection decorators in ApiV1 class

--- src/api/v1.ts
+++ src/api/v1.ts
@@ -1,18 +1,16 @@
 // import { Router } from "express";
 
-import { Dependency } from '@/common/di';
+import { dependency } from '@/common/di';
 import type { ApiReq, ApiRes } from '@/common/http';
 import { HttpStatus } from '@/common/http';
 import type { Application } from 'express';
 import { Router } from 'express';
-import { injectable } from 'inversify';
 
 import { apiFunctions } from './client';
 type Versions = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 export type ApiVersions = `/api/v${Versions}`;
 
-@injectable()
-@Dependency()
+@dependency()
 export class ApiV1 {
   protected router: Router;
   version: ApiVersions;
