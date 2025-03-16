af7c53b277a64b015b3270d684e7e6da65a68c07
Felix Orinda <forinda82@gmail.com>
2025-03-16 10:13:07 +0300

    Refactor controllers to remove unused dependency injection decorators

--- src/api/client/projects/controllers/remove-users.controller.ts
+++ src/api/client/projects/controllers/remove-users.controller.ts
@@ -3,9 +3,8 @@ import {
   Controller,
   ApiControllerMethod
 } from '@/common/decorators/controller.decorator';
-import { Dependency } from '@/common/di';
 import type { ApiRequestContext } from '@/common/interfaces/controller';
-import { inject, injectable } from 'inversify';
+import { inject } from 'inversify';
 
 import type { RemoveUsersFromProjectPayload } from '../schema/schema';
 import { removeUsersFromProjectSchema } from '../schema/schema';
@@ -13,8 +12,6 @@ import { userAudit } from '@/common/utils/user-request-audit';
 import { RemoveUserFromProjectService } from '../services/remove-users.service';
 import { createHttpResponse } from '@/common/utils/responder';
 
-@injectable()
-@Dependency()
 @Controller()
 export class RemoveUserFromProjectController extends BasePostController {
   @inject(RemoveUserFromProjectService)
