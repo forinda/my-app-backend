ea4b0dc436322498baa6da57a114600c1e0c4c9a
Felix Orinda <forinda82@gmail.com>
2025-03-17 09:44:14 +0300

    Refactor avatar utility to use Avatar class; add method for generating organization logos

--- src/common/utils/avatar.ts
+++ src/common/utils/avatar.ts
@@ -1,3 +1,19 @@
-export function generateAvatar(first_name: string, last_name: string) {
-  return `https://avatar.iran.liara.run/username?username=${first_name}+${last_name}`;
+import { dependency } from '../di';
+import crypto from 'crypto';
+// export function generateAvatar(first_name: string, last_name: string) {
+//   return `https://avatar.iran.liara.run/username?username=${first_name}+${last_name}`;
+// }
+
+@dependency()
+export class Avatar {
+  generateAvatar(first_name: string, last_name: string) {
+    return `https://avatar.iran.liara.run/username?username=${first_name}+${last_name}`;
+  }
+
+  generateOrgLogo(name: string) {
+    const orgName = name.replace(/\s/g, '+');
+    const md5Hash = crypto.createHash('md5').update(orgName).digest('hex');
+
+    return `https://robohash.org/${md5Hash}?gravatar=hashed&size=500x500&bgset=bg1&ignoreext=false`;
+  }
 }
