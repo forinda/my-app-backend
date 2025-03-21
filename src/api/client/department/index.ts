31bbbad787603f74ab454353585179994ccaf6fa
Felix Orinda <forinda82@gmail.com>
2025-03-21 06:29:50 +0300

    Add route to fetch members of a department

--- src/api/client/department/index.ts
+++ src/api/client/department/index.ts
@@ -6,6 +6,7 @@ import { FetchDepartmentsController } from './controllers/fetch.controller';
 import { AddUserToDepartmentController } from './controllers/add-users.controller';
 import type { RouteSetupFunction } from '@/common/interfaces/controller';
 import { FetchDepartmentByIdController } from './controllers/fetch-by-id.controller';
+import { FetchDepartmentMembersController } from './controllers/fetch-members.controller';
 
 export const setupDepartmentRoutes: RouteSetupFunction = ({ app }) => {
   const router = Router();
@@ -14,6 +15,7 @@ export const setupDepartmentRoutes: RouteSetupFunction = ({ app }) => {
     .get('/', di.resolve(FetchDepartmentsController).get)
     .post('/', di.resolve(NewDepartmentController).post)
     .get('/:id', di.resolve(FetchDepartmentByIdController).get)
+    .get('/:id/members', di.resolve(FetchDepartmentMembersController).get)
     .put('/:id', di.resolve(UpdateDepartmentController).put)
     .post('/add-member/:id', di.resolve(AddUserToDepartmentController).post);
 
