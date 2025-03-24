4d12a25e2983e6a0cb136d8f7aef00c89798cec6
Felix Orinda <forinda82@gmail.com>
2025-03-24 08:36:45 +0300

    Enhance FetchProjectsService to validate query parameters and apply filters in get method

--- src/api/client/projects/services/fetch.service.ts
+++ src/api/client/projects/services/fetch.service.ts
@@ -3,25 +3,63 @@ import { useDrizzle } from '@/db';
 import { HttpStatus } from '@/common/http';
 import { dependency } from '@/common/di';
 import type { ApiPaginationParams } from '@/common/utils/pagination';
-import { asc, eq } from 'drizzle-orm';
+import { and, asc, eq, ilike, or } from 'drizzle-orm';
+import type { BareObject } from '@/common/interfaces/helpers';
+import { inject } from 'inversify';
+import { SchemaValidator } from '@/common/schema/validator';
+import { fetchProjectCategoryQuerySchema } from '../schema/schema';
 
 @dependency()
 export class FetchProjectsService {
-  async get(organization_id: number, _?: ApiPaginationParams) {
-    const db = useDrizzle();
+  @inject(SchemaValidator) private validator: SchemaValidator;
 
-    const depts = await db.query.OrgProject.findMany({
-      where: eq(OrgProject.organization_id, organization_id),
-      with: {
-        members: {}
-      },
-      limit: _?.limit,
-      offset: _?.offset,
+  async get(
+    organization_id: number,
+    query: BareObject,
+    pagination?: ApiPaginationParams
+  ) {
+    const db = useDrizzle();
+    const { limit, offset } = pagination || {};
+
+    // Validate and extract filter options from the query
+    const filters = this.validator.validate(
+      fetchProjectCategoryQuerySchema,
+      query
+    );
+    const filterOptions = [];
+
+    if (filters?.category_id) {
+      filterOptions.push(eq(OrgProject.category_id, filters.category_id));
+    }
+
+    if (filters?.q) {
+      const searchQuery = `%${filters.q}%`;
+
+      filterOptions.push(
+        or(
+          ilike(OrgProject.name, searchQuery),
+          ilike(OrgProject.description, searchQuery)
+        )
+      );
+    }
+
+    // Build the final filter condition
+    const finalFilter = and(
+      eq(OrgProject.organization_id, organization_id),
+      ...filterOptions
+    );
+
+    // Fetch projects with pagination and related members
+    const projects = await db.query.OrgProject.findMany({
+      where: finalFilter,
+      with: { members: true },
+      limit,
+      offset,
       orderBy: [asc(OrgProject.created_at)]
     });
 
     return {
-      data: depts,
+      data: projects,
       message: 'Projects fetched successfully',
       status: HttpStatus.OK
     };
