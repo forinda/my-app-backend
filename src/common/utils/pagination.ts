1b7ef9a75ecc0f291c1c48f88c8ddf20896cb0ca
Felix Orinda <forinda82@gmail.com>
2025-03-28 09:31:10 +0300

    Add support for 'all' query parameter in pagination utility to return all records

--- src/common/utils/pagination.ts
+++ src/common/utils/pagination.ts
@@ -2,31 +2,42 @@ import type { ApiReq } from '../http';
 import { convertToNumber } from './numbers';
 
 export function extractPaginationParams(query: ApiReq['query']) {
-  const { page: pageQuery, limit: limitQuery } = query;
+  const { page: pageQuery, limit: limitQuery, all: queryAll } = query;
   const page = pageQuery ? convertToNumber(pageQuery as string) : 1;
   const limit = limitQuery ? convertToNumber(limitQuery as string) : 100;
+
+  if (queryAll) {
+    return { page: 1, limit: undefined, all: true, offset: 0 };
+  }
 
   delete query.page;
   delete query.limit;
   const offset = (page - 1) * limit;
 
-  return { page, limit, offset };
+  return { page, limit, offset, all: false };
 }
 
 export function paginator<T = unknown>(
   data: T[],
   total: number,
-  { limit, page }: ApiPaginationParams
+  { limit, page, all }: ApiPaginationParams
 ) {
   return {
     data,
     meta: {
-      pagination: {
-        total,
-        limit,
-        page,
-        pages: Math.ceil(total / limit)
-      }
+      pagination: all
+        ? {
+            total,
+            limit: total,
+            page,
+            pages: 1
+          }
+        : {
+            total,
+            limit,
+            page,
+            pages: Math.ceil(total / limit!)
+          }
     }
   };
 }
