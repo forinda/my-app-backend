df5e4598a645cef2297946e83dc632d1f5b12ec2
Felix Orinda <forinda82@gmail.com>
2025-03-29 23:35:08 +0300

    Add unassign task route and clean up comment routes in task API

--- src/api/client/tasks/index.ts
+++ src/api/client/tasks/index.ts
@@ -8,6 +8,7 @@ import type { RouteSetupFunction } from '@/common/interfaces/controller';
 import { AddTaskCommentController } from './controllers/add-comment.controller';
 import { UpdateTaskCommentController } from './controllers/update-comment.controller';
 import { FetchTaskByIdController } from './controllers/fetch-by-id.controller';
+import { UnAssignTaskController } from './controllers/unassign.controller';
 
 export const setupTaskRoutes: RouteSetupFunction = ({ app }) => {
   const router = Router();
@@ -15,11 +16,12 @@ export const setupTaskRoutes: RouteSetupFunction = ({ app }) => {
   router
     .get('/', di.resolve(FetchTaskController).get)
     .post('/', di.resolve(CreateTaskController).post)
+    .post('/add-comment', di.resolve(AddTaskCommentController).put)
+    .put('/update-comment', di.resolve(UpdateTaskCommentController).put)
     .put('/:id', di.resolve(UpdateTaskController).put)
     .get('/:id', di.resolve(FetchTaskByIdController).get)
     .patch('/:id/assign', di.resolve(AssignTaskController).post)
-    .post('/add-comment', di.resolve(AddTaskCommentController).put)
-    .put('/update-comment', di.resolve(UpdateTaskCommentController).put);
+    .patch('/:id/unassign', di.resolve(UnAssignTaskController).post);
 
   app.use('/tasks', router);
 };
